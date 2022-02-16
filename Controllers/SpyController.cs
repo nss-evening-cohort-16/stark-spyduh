255b6883fea0e4d80c950b7a6aa5c1eb5d20c5d1
Dario Perez <darioperez1415@gmail.com>
2022-02-16 00:10:11 -0600

    Added Model, Conroler for Spies

---
+++ Controllers/SpyController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using StarkSpyduh.Models;
+
+namespace StarkSpyduh.Controllers
+{
+    [Route("api/spy")]
+    [ApiController]
+    public class SpyController : Controller
+    {
+        private static List<Spy> _Spies = new List<Spy>()
+        {
+        new Spy()
+        {
+            Name = "JmaesBond",
+            Id = 1,
+            Friend = true,
+            Enemy = false,
+            Skill = SkillsType.None
+        },
+        new Spy()
+        {
+            Name = "Casper",
+            Id = 2,
+            Friend = false,
+            Enemy = true,
+            Skill = SkillsType.Hacking
+        },
+        new Spy(){
+            Name = "Basil",
+            Id = 3,
+            Friend = true,
+            Enemy = false,
+            Skill = SkillsType.MoneyLoundering
+        },
+        new Spy()
+        {
+            Name = "Hunter",
+            Id = 4,
+            Friend = false,
+            Enemy = true,
+            Skill = SkillsType.Wiretapping
+        },
+        new Spy()
+        {
+            Name = "Moonlight",
+            Id = 5,
+            Friend = false,
+            Enemy= true,
+            Skill = SkillsType.Whistleblower
+        },
+        };
+        [HttpGet]
+        public List<Spy> GetAllSpies()
+        {
+            return _Spies;
+        }
+        [HttpPost]
+        public IActionResult AddNewSpy(Spy newSpy)
+        {
+            _Spies.Add(newSpy);
+
+            return Ok(newSpy);
+        }
+    }
+}
---
+++ Models/Spy.cs
@@ -0,0 +1,24 @@
+﻿namespace StarkSpyduh.Models
+{
+    public class Spy
+    {
+        public string Name { get; set; }
+
+        public int Id { get; set; }
+
+        public bool Friend { get; set; }
+
+        public bool Enemy { get; set; }
+
+        public SkillsType Skill { get; set; }
+
+    }
+    public enum SkillsType
+    {
+        None, 
+        Wiretapping, 
+        Hacking, 
+        MoneyLoundering, 
+        Whistleblower,
+    }
+}
